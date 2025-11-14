60705fb3c62544f3f528c0254bd6a3d3920bad94
Viktor Grozan <82977659+MarkysLeet@users.noreply.github.com>
2025-11-14 13:16:43 +0300

    Updated app-build-manifest.json

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/project/app/layout\":[\"static/media/6cca48c3e4e6ef07-s.p.woff2\",\"static/media/45d262064560f5fa-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
